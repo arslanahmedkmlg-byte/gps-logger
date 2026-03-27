6edc64684a8db956a06a0b4125d23998608aa018
arslanahmedkmlg-byte <arslanahmed.kmlg@gmail.com>
2026-03-27 19:30:36 +0500

    Update BootReceiver.kt

--- app/src/main/java/com/ars/gpslogger/BootReceiver.kt
+++ app/src/main/java/com/ars/gpslogger/BootReceiver.kt
@@ -7,13 +7,16 @@ import android.os.Build
 
 class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
-        if (intent.action == Intent.ACTION_BOOT_COMPLETED ||
-            intent.action == "android.intent.action.QUICKBOOT_POWERON") {
-            val service = Intent(context, GpsLoggerService::class.java)
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
-                context.startForegroundService(service)
-            else
-                context.startService(service)
+
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+
+            val serviceIntent = Intent(context, GpsLoggerService::class.java)
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                context.startForegroundService(serviceIntent)
+            } else {
+                context.startService(serviceIntent)
+            }
         }
     }
 }
